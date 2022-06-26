828156ccd67f35565019f03cb2eb487b8fd59933
S-B-A <sasha.berg.agency@gmail.com>
2022-06-26 22:53:48 +0300

    Working with INT numbers.

--- src/arraymethoddemo/ArrayMethodDemo.java
+++ src/arraymethoddemo/ArrayMethodDemo.java
@@ -1,16 +1,21 @@
 package arraymethoddemo;
 import java.util.Arrays;
+import java.util.Scanner;
+
 class MyClass{
-    public void printFirstElement(float[] a)
+    /* public void printFirstElement(float[] a)
 {
 System.out.println("The first element is " + a[0]);
 }
-    public int[] returnArray()
+
+     */
+    public int[] returnArray(int k)
             {
-                int[] a = new int[3];
+                int[] a = new int[k];
                 for (int i = 0; i < a.length; i++)
                 {
-                    a[i] = i*2;
+                    Scanner input = new Scanner(System.in);
+                    a[i] = input.nextInt();
                 }
                 return a;
             }
@@ -18,9 +23,9 @@ System.out.println("The first element is " + a[0]);
 public class ArrayMethodDemo {
     public static void main(String[] args) {
         MyClass amd = new MyClass();
-        float[] myArray = {1.5F, 2F, 3.75F, 4.15F, 5.24F};
-        amd.printFirstElement(myArray);
-        int[] myArray2 = amd.returnArray();
+        //float[] myArray = {1.5F, 2F, 3.75F, 4.15F, 5.24F};
+        // amd.printFirstElement(myArray);
+        int[] myArray2 = amd.returnArray(5);
         System.out.println(Arrays.toString(myArray2));
     }
 }
