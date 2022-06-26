de87256153b6dadc135f2f2f3d38cd1329f7734d
S-B-A <sasha.berg.agency@gmail.com>
2022-06-26 21:44:59 +0300

    Empty line deleted.

--- src/arraymethoddemo/ArrayMethodDemo.java
+++ src/arraymethoddemo/ArrayMethodDemo.java
@@ -1,14 +1,10 @@
 package arraymethoddemo;
-
 import java.util.Arrays;
-
 class MyClass{
-
 public void printFirstElement(float[] a)
 {
 System.out.println("The first element is " + a[0]);
 }
-
 public int[] returnArray()
 {
 int[] a = new int[3];
@@ -18,19 +14,13 @@ a[i] = i*2;
 }
 return a;
 }
-
 }
-
 public class ArrayMethodDemo {
 public static void main(String[] args) {
-
 MyClass amd = new MyClass();
-
 float[] myArray = {1.5F, 2F, 3.75F, 4.15F, 5.24F};
 amd.printFirstElement(myArray);
-
 int[] myArray2 = amd.returnArray();
 System.out.println(Arrays.toString(myArray2));
-
 }
 }
